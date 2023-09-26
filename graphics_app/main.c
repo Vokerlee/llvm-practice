60e751211417bdc082c4e52d6963f5b6d281c490
Vokerlee <vokerlee@gmail.com>
2023-09-26 23:39:07 +0300

    Add simple drawing example in GTK window

--- graphics_app/main.c
+++ graphics_app/main.c
@@ -1,16 +1,62 @@
+// #include <cairo.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
 
-static void activate (GtkApplication* app, gpointer user_data)
+#define WIDTH_DEFAULT  1000
+#define HEIGHT_DEFAULT 700
+#define BYTES_PER_R8G8B8 3
+
+static void fill_row(GByteArray *array, guint8 value, int row_size)
 {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
-    gtk_window_present(window);
+    for (guint i = 0; i < row_size; i++)
+    {
+        g_byte_array_append(array, &value, 1); // R
+        g_byte_array_append(array, &value, 1); // G
+        g_byte_array_append(array, &value, 1); // B
+    }
 }
 
-int main (int argc, char *argv[])
+static void add_pixel_picture(GtkPicture *picture)
 {
-    GtkApplication *app = gtk_application_new("graphics_app", G_APPLICATION_DEFAULT_FLAGS);
+    GByteArray *pixels = g_byte_array_new();
+
+    for (guint i = 0; i <= HEIGHT_DEFAULT; i++)
+        fill_row(pixels, 128, WIDTH_DEFAULT);
+
+    gsize height = pixels->len / (WIDTH_DEFAULT * BYTES_PER_R8G8B8);
+    g_autoptr(GBytes) bytes = g_byte_array_free_to_bytes(pixels);
+
+    GdkTexture *texture = gdk_memory_texture_new(WIDTH_DEFAULT, height,
+                                        GDK_MEMORY_R8G8B8,
+                                        bytes,
+                                        WIDTH_DEFAULT * BYTES_PER_R8G8B8);
+    gtk_picture_set_paintable(picture, GDK_PAINTABLE(texture));
+}
+
+static void activate(GtkApplication* app, gpointer user_data)
+{
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Graphics app");
+    gtk_window_set_default_size(GTK_WINDOW(window), WIDTH_DEFAULT, HEIGHT_DEFAULT);
+
+    GtkWidget *picture = gtk_picture_new();
+    gtk_widget_add_css_class(picture, "frame");
+    g_object_set(picture,
+                "margin-start",  0,
+                "margin-end",    0,
+                "margin-top",    0,
+                "margin-bottom", 0,
+                NULL);
+
+    gtk_window_set_child(GTK_WINDOW(window), picture);
+    add_pixel_picture(GTK_PICTURE(picture));
+
+    gtk_window_present(GTK_WINDOW(window));
+}
+
+int main(int argc, char *argv[])
+{
+    GtkApplication *app = gtk_application_new("graph.app", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
