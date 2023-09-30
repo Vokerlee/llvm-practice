d3e0fedf27069123b57e1523a6b599c1175169b1
Vokerlee <vokerlee@gmail.com>
2023-10-01 00:21:37 +0300

    Disable window resize to eliminare SIGSEGV in case of its expansion

--- graphics_app/sgl/sgl.c
+++ graphics_app/sgl/sgl.c
@@ -157,6 +157,7 @@ static void activate(GtkApplication *app, gpointer data)
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Graphics app");
     gtk_window_set_default_size(GTK_WINDOW(window), win_info->width, win_info->height);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
     GtkWidget *frame = gtk_frame_new(NULL);
     gtk_window_set_child(GTK_WINDOW(window), frame);
