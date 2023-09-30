7ae93e35c9643f32233fecd955cb15840aa83fb3
Vokerlee <vokerlee@gmail.com>
2023-10-01 00:14:02 +0300

    Fix error in gameoflife source file

--- graphics_app/gameoflife.c
+++ graphics_app/gameoflife.c
@@ -1,9 +1,8 @@
 #include "sgl_rand.h"
-#include <stdio.h>
 
 void init_func(unsigned char *pixels_old, unsigned char *pixels_new, int width, int height)
 {
-    int seed = 732;
+    int seed = -21731286;
     sgl_srand(seed);
 
     for (int y = 0; y < height; y++)
@@ -29,7 +28,7 @@ void draw_func(unsigned char *pixels_old, unsigned char *pixels_new, int width, 
         for (int j = 0; j < width; j++)
         {
             int index = (i * width + j) * 4;
-            int neighbors = 0;
+            int n_neighbors = 0;
 
             for (int y = -1; y <= 1; y++)
             {
@@ -38,19 +37,17 @@ void draw_func(unsigned char *pixels_old, unsigned char *pixels_new, int width, 
                     if (x == 0 && y == 0)
                         continue;
 
-                    int nindex = (((i + y) % height) * width + ((j + x) % width)) * 4;
+                    int nindex = (((i + y + height) % height) * width + ((j + x + width) % width)) * 4;
 
                     if (nindex >= 0 && nindex < (width * height * 4 - 1))
                         if (pixels_old[nindex + 3] != 0)
-                            neighbors += 1;
+                            n_neighbors += 1;
                 }
             }
 
-            // printf("%d\n", neighbors);
-
-            if (pixels_old[index + 3] == 255 && (neighbors < 2 || neighbors > 3))
+            if (pixels_old[index + 3] == 255 && (n_neighbors < 2 || n_neighbors > 3))
                 pixels_new[index + 3] = 0;
-            else if (pixels_old[index] == 0 && neighbors == 3)
+            else if (pixels_old[index + 3] == 0 && n_neighbors == 3)
                 pixels_new[index + 3] = 255;
             else
                 pixels_new[index + 3] = pixels_old[index + 3];
