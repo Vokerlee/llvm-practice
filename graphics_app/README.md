50741dc681d833ccd4723a2495387449b86c2dc3
Vokerlee <vokerlee@gmail.com>
2023-10-23 01:32:28 +0300

    Minor changes in README.md, fix minor bugs

--- graphics_app/README.md
+++ graphics_app/README.md
@@ -10,9 +10,25 @@
 
 ## How to build & launch
 
-  * Build `Game of Life`:
+  * Build `Game of Life` & launch:
     * ```shell
       mkdir -p ./gameoflife_sdl/build && cd ./gameoflife_sdl/build
       cmake ..
       make
+      ./gameoflife
       ```
+  * Build LLVM Logger Pass:
+    * ```shell
+      cd ./llvm_pass_logger
+      make
+      ```
+  * Build patched `Game of Life` with LLVM Logger Pass & launch:
+    * ```shell
+      make
+      ./build/gameoflife > filename.trace
+      ```
+  * Get LLVM IR operations frequency analyses:
+    * ```shell
+      python3 trace_analyses/gen_plots.py filename.trace
+      ```
+
--- graphics_app/trace_analyses/gen_plots.py
+++ graphics_app/trace_analyses/gen_plots.py
@@ -71,35 +71,35 @@ if __name__ == '__main__':
 
     n_top_combinations = 10
 
-    # comb_2 = []
-    # for i in range(0, len(instructions) - 1):
-    #     comb_2.append((instructions[i], instructions[i + 1]))
+    comb_2 = []
+    for i in range(0, len(instructions) - 1):
+        comb_2.append((instructions[i], instructions[i + 1]))
 
-    # comb_3 = []
-    # for i in range(0, len(instructions) - 2):
-    #     comb_3.append((instructions[i], instructions[i + 1], instructions[i + 2]))
+    comb_3 = []
+    for i in range(0, len(instructions) - 2):
+        comb_3.append((instructions[i], instructions[i + 1], instructions[i + 2]))
 
-    # comb_4 = []
-    # for i in range(0, len(instructions) - 3):
-    #     comb_4.append((instructions[i], instructions[i + 1], instructions[i + 2], instructions[i + 3]))
+    comb_4 = []
+    for i in range(0, len(instructions) - 3):
+        comb_4.append((instructions[i], instructions[i + 1], instructions[i + 2], instructions[i + 3]))
 
     comb_5 = []
     for i in range(0, len(instructions) - 4):
         comb_5.append((instructions[i], instructions[i + 1], instructions[i + 2], instructions[i + 3], instructions[i + 4]))
 
-    # unique_instr  = sorted({comb : instructions.count(comb) for comb in set(instructions)}.items(), \
-    #                        key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    # unique_comb_2 = sorted({comb : comb_2.count(comb) for comb in set(comb_2)}.items(), \
-    #                        key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    # unique_comb_3 = sorted({comb : comb_3.count(comb) for comb in set(comb_3)}.items(), \
-    #                        key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    # unique_comb_4 = sorted({comb : comb_4.count(comb) for comb in set(comb_4)}.items(), \
-    #                        key=lambda item: item[1], reverse=True)[0:n_top_combinations]
+    unique_instr  = sorted({comb : instructions.count(comb) for comb in set(instructions)}.items(), \
+                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
+    unique_comb_2 = sorted({comb : comb_2.count(comb) for comb in set(comb_2)}.items(), \
+                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
+    unique_comb_3 = sorted({comb : comb_3.count(comb) for comb in set(comb_3)}.items(), \
+                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
+    unique_comb_4 = sorted({comb : comb_4.count(comb) for comb in set(comb_4)}.items(), \
+                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
     unique_comb_5 = sorted({comb : comb_5.count(comb) for comb in set(comb_5)}.items(), \
                            key=lambda item: item[1], reverse=True)[0:n_top_combinations]
 
-    # build_plot("10 most used combination of instructions (1 in a row)", unique_instr, "comb_1.png")
-    # build_plot("10 most used combination of instructions (2 in a row)", unique_comb_2, "comb_2.png")
-    # build_plot("10 most used combination of instructions (3 in a row)", unique_comb_3, "comb_3.png")
-    # build_plot("10 most used combination of instructions (4 in a row)", unique_comb_4, "comb_4.png")
+    build_plot("10 most used combination of instructions (1 in a row)", unique_instr, "comb_1.png")
+    build_plot("10 most used combination of instructions (2 in a row)", unique_comb_2, "comb_2.png")
+    build_plot("10 most used combination of instructions (3 in a row)", unique_comb_3, "comb_3.png")
+    build_plot("10 most used combination of instructions (4 in a row)", unique_comb_4, "comb_4.png")
     build_plot("10 most used combination of instructions (5 in a row)", unique_comb_5, "comb_5.png")
