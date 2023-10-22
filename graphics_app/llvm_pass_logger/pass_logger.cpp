69967f8608ba2ea6a45bcfdd1c3e0e804a3a3e72
Vokerlee <vokerlee@gmail.com>
2023-10-22 22:28:56 +0300

    Convert tabs to spaces in LLVM logger pass

--- graphics_app/llvm_pass_logger/pass_logger.cpp
+++ graphics_app/llvm_pass_logger/pass_logger.cpp
@@ -19,21 +19,21 @@ struct SimpleLogger : public FunctionPass
     SimpleLogger() : FunctionPass(ID) {}
 
     bool isFuncLogger(StringRef name)
-	{
+    {
         return name == "op_logger" || name == "call_logger";
     }
 
     virtual bool runOnFunction(Function &F)
-	{
+    {
         if (isFuncLogger(F.getName()))
             return false;
 
         outs() << "In a function called " << F.getName() << "\n\n";
 
         for (auto &B : F)
-		{
+        {
             for (auto &I : B)
-			{
+            {
                 outs() << "Instruction: " << (uint64_t)(&I) << "\n";
                 I.print(outs(), true);
                 outs() << "\n";
@@ -55,13 +55,13 @@ struct SimpleLogger : public FunctionPass
             F.getParent()->getOrInsertFunction("op_logger", loggerType);
 
         FunctionCallee callLogFunc =
-			F.getParent()->getOrInsertFunction("call_logger", loggerType);
+            F.getParent()->getOrInsertFunction("call_logger", loggerType);
 
         // Insert loggers for call, binOpt and ret instructions
         for (auto &B : F)
-		{
+        {
             for (auto &I : B)
-			{
+            {
                 builder.SetInsertPoint(&I);
 
                 if (auto *phi = dyn_cast<PHINode>(&I))
