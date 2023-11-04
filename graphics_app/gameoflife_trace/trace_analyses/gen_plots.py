37e3c3d3be08e9dbe4a67e0ff2152d4d5b5b0031
Vokerlee <vokerlee@gmail.com>
2023-11-04 19:34:13 +0300

    Add more combinations of LLVM IR ops, optimise plot-generation script

--- graphics_app/gameoflife_trace/trace_analyses/gen_plots.py
+++ graphics_app/gameoflife_trace/trace_analyses/gen_plots.py
@@ -43,8 +43,8 @@ def build_plot(title, instructions, savename):
     gs = GridSpec(ncols=1, nrows=1, figure=figure)
     ax = figure.add_subplot(gs[0, 0])
 
-    ax.set_xlabel("Amount of uses")
-    ax.set_xlabel("Operation")
+    ax.set_xlabel("Amount of uses (relative)")
+    ax.set_ylabel("Operation")
     ax.set_title(title)
 
     set_axis_properties(ax)
@@ -60,6 +60,10 @@ if __name__ == '__main__':
         print(f"Usage: {sys.argv[0]} <filname.trace>", file=sys.stderr)
         exit(1)
 
+    n_top_combinations = 12
+    max_comb_start = 2
+    max_comb_size  = 9
+
     trace_filename = sys.argv[1]
     instructions = []
 
@@ -69,37 +73,29 @@ if __name__ == '__main__':
     for i in range(0, len(instructions)):
         instructions[i] = instructions[i].strip()
 
-    n_top_combinations = 10
-
-    comb_2 = []
-    for i in range(0, len(instructions) - 1):
-        comb_2.append((instructions[i], instructions[i + 1]))
-
-    comb_3 = []
-    for i in range(0, len(instructions) - 2):
-        comb_3.append((instructions[i], instructions[i + 1], instructions[i + 2]))
-
-    comb_4 = []
-    for i in range(0, len(instructions) - 3):
-        comb_4.append((instructions[i], instructions[i + 1], instructions[i + 2], instructions[i + 3]))
-
-    comb_5 = []
-    for i in range(0, len(instructions) - 4):
-        comb_5.append((instructions[i], instructions[i + 1], instructions[i + 2], instructions[i + 3], instructions[i + 4]))
-
-    unique_instr  = sorted({comb : instructions.count(comb) for comb in set(instructions)}.items(), \
-                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    unique_comb_2 = sorted({comb : comb_2.count(comb) for comb in set(comb_2)}.items(), \
-                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    unique_comb_3 = sorted({comb : comb_3.count(comb) for comb in set(comb_3)}.items(), \
-                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    unique_comb_4 = sorted({comb : comb_4.count(comb) for comb in set(comb_4)}.items(), \
-                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
-    unique_comb_5 = sorted({comb : comb_5.count(comb) for comb in set(comb_5)}.items(), \
+    unique_instr = sorted({comb : instructions.count(comb) / len(instructions) for comb in set(instructions)}.items(), \
                            key=lambda item: item[1], reverse=True)[0:n_top_combinations]
 
-    build_plot("10 most used combination of instructions (1 in a row)", unique_instr, "comb_1.png")
-    build_plot("10 most used combination of instructions (2 in a row)", unique_comb_2, "comb_2.png")
-    build_plot("10 most used combination of instructions (3 in a row)", unique_comb_3, "comb_3.png")
-    build_plot("10 most used combination of instructions (4 in a row)", unique_comb_4, "comb_4.png")
-    build_plot("10 most used combination of instructions (5 in a row)", unique_comb_5, "comb_5.png")
+    all_combs = []
+
+    for comb_size in range(max_comb_start, max_comb_size + 1):
+        combs = []
+
+        for i in range(0, len(instructions) - comb_size + 1):
+            comb = []
+            for j in range(comb_size):
+                comb.append(instructions[i + j])
+
+            combs.append(tuple(comb))
+
+        n_combs = len(combs)
+
+        combs_sorted = sorted({comb : combs.count(comb) / n_combs for comb in set(combs)}.items(), \
+                           key=lambda item: item[1], reverse=True)[0:n_top_combinations]
+
+        all_combs.append(combs_sorted)
+
+    build_plot(f"10 most used combination of instructions (1 in a row)", unique_instr, "comb_1.png")
+
+    for comb_size in range(max_comb_start, max_comb_size + 1):
+        build_plot(f"10 most used combination of instructions ({comb_size} in a row)", all_combs[comb_size - max_comb_start], f"comb_{comb_size}.png")
