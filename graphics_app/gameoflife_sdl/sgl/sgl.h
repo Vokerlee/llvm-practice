8e235a8f5317075c2e9b11e5bab21ce89c5d8eee
Vokerlee <vokerlee@gmail.com>
2023-11-04 18:18:54 +0300

    Replace gameoflife LLVM IR patching to subdirectory

--- graphics_app/gameoflife_sdl/sgl/sgl.h
+++ graphics_app/gameoflife_sdl/sgl/sgl.h
@@ -1,19 +1,9 @@
 #ifndef SGL_INCLUDE_SGL_INCLUDE
 #define SGL_INCLUDE_SGL_INCLUDE
 
-#define SGL_WIDTH_DEFAULT  1080
-#define SGL_HEIGHT_DEFAULT 720
+#include "sgl_minimal.h"
 
 int  sgl_initialize();
 void sgl_close();
-
-int  sgl_get_pixel(int x, int y);
-void sgl_put_pixel(int x, int y, int argb);
-
-void sgl_update(int pixel_buffer[SGL_HEIGHT_DEFAULT][SGL_WIDTH_DEFAULT]);
-void sgl_flush();
-
-int  sgl_rand();
-void sgl_srand(int seed);
 
 #endif // SGL_INCLUDE_SGL_INCLUDE
