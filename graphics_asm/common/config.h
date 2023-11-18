b55f3a52f5f4f25531e644af81bde5e8e22fd728
Vokerlee <vokerlee@gmail.com>
2023-11-18 23:57:51 +0300

    Add config with register types

---
+++ graphics_asm/common/config.h
@@ -0,0 +1,14 @@
+#ifndef GRASM_COMMON_CONFIG_H
+#define GRASM_COMMON_CONFIG_H
+
+#include <cstdint>
+
+namespace grasm
+{
+
+using RegIdx = uint8_t;
+using Reg    = int64_t;
+
+}; // grasm
+
+#endif // GRASM_COMMON_CONFIG_H
