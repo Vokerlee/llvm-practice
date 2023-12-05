32659b4b6adb5c4ac6df87673fd5eb527f267bd5
Vokerlee <vokerlee@gmail.com>
2023-12-05 16:27:28 +0300

    Add common folder for language directory

---
+++ graphics_lang2ir/common/config.h
@@ -0,0 +1,13 @@
+#ifndef GRLANG_COMMON_CONFIG_H
+#define GRLANG_COMMON_CONFIG_H
+
+#include <cstdint>
+
+namespace grlang
+{
+
+using Int = int64_t;
+
+}; // grlang
+
+#endif // GRLANG_COMMON_CONFIG_H
