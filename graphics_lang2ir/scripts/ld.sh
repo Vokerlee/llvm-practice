bb4ba35b7d118e595a46b84ec1cf099a030c8a4c
Vokerlee <vokerlee@gmail.com>
2023-12-08 02:41:57 +0300

    Update script with grlang file compilation & linking

--- graphics_lang2ir/scripts/ld.sh
+++ graphics_lang2ir/scripts/ld.sh
@@ -6,16 +6,17 @@ src_file=$1
 build_path=$2
 output_path=$3
 
-output_ll_file="${src_file##*/}".ll
-output_asm_file="${src_file##*/}".s
-output_obj_file="${src_file##*/}".o
+src_file_noext="${src_file##*/}"
+src_file_noext=$(echo ${src_file_noext} | cut -f -1 -d '.')
 
+output_ll_file=${src_file_noext}.ll
 ./${build_path}/bin/grlang < ${src_file} > ${output_path}/${output_ll_file}
 
-src_file="${src_file##*/}"
-src_file=$(echo ${src_file} | cut -f -1 -d '.')
-output_file="${src_file##*/}".out
-
+output_asm_file=${src_file_noext}.s
 llc ${output_path}/${output_ll_file} -o ${output_path}/${output_asm_file}
+
+output_obj_file=${src_file_noext}.o
 clang++ -c ${output_path}/${output_asm_file} -o ${output_path}/${output_obj_file}
-clang++ ${output_path}/${output_obj_file} ${output_path}/${grlang_stdlib_rel_path} -lLLVM -o ${output_path}/${output_file}
+
+output_file=${src_file_noext}.out
+clang++ ${output_path}/${output_obj_file} ${output_path}/${grlang_stdlib_rel_path} -lLLVM -lSDL2 -o ${output_path}/${output_file}
